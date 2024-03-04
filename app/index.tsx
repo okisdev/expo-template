4b6395dbdc4ee3c0110946eda85e4aa5a6b0d737
Harry Yep <git@hode.co.uk>
2024-03-04 14:32:20 +0800

    feat: screens

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,11 @@
+import { Link } from 'expo-router';
 import { View, Text } from 'react-native';
 
 export default function App() {
     return (
         <View>
             <Text>Open up App.tsx to start working on your app!</Text>
+            <Link href='/about'>Go to About</Link>
         </View>
     );
 }
