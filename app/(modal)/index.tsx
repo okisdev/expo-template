aa9a620adee3ebe8e630c02c47846fd492166973
Harry Yep <git@hode.co.uk>
2024-03-04 15:10:07 +0800

    feat: quit modal

--- app/(modal)/index.tsx
+++ app/(modal)/index.tsx
@@ -1,11 +1,16 @@
-import { Link } from 'expo-router';
-import { View, Text } from 'react-native';
+import { Link, useRouter } from 'expo-router';
+import { View, Text, Pressable } from 'react-native';
 
 export default function ModalIndex() {
+    const router = useRouter();
+
     return (
         <View>
             <Text>Modal Index</Text>
             <Link href='./modal-a'>Go Modal A</Link>
+            <Pressable onPress={() => router.back()}>
+                <Text>Quit Modal</Text>
+            </Pressable>
         </View>
     );
 }
